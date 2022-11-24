fea26c9fde9cd5e08d9cbe7ac8d768075b7aa3ab
katharin_termux <e.semenuta99@gmail.com>
2022-11-24 11:54:41 +0400

    kartharin_termux: started regex

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,4 +1,15 @@
 #include "s21_grep_funcs.h"
+#include <regex.h>
+
+void print_result(int value) {
+	if (value == 0) {
+		printf("Pattern found\n");
+	} else if (value == REG_NOMATCH) {
+		printf("Pattern not found\n");
+	} else {
+		printf("An error occured\n");
+	}
+}
 
 int main(int argc, char **argv) {
   const char *short_options = "eivclnhsfo";
